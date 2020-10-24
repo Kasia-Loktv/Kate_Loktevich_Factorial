2a1cf2c69b65fbae716234a11477cdd477db29ef
Kasia-Loktv <73357983+Kasia-Loktv@users.noreply.github.com>
2020-10-24 20:16:26 +0300

    added .gitignore and projects files

---
+++ Factorial/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n;
+            bool result = int.TryParse(Console.ReadLine(), out n);
+            while (result == false)
+            {
+                Console.WriteLine("Перепроверьте число и введите снова");
+                result = int.TryParse(Console.ReadLine(), out n);
+            }
+
+            int[] fact = new int[1000];
+
+
+            fact[0] = 1;
+            int size = 1;
+
+
+            for (int x = 2; x <= n; x++)
+            {
+                int carry = 0;
+
+
+                for (int i = 0; i < size; i++)
+                {
+                    int prod = fact[i] * x + carry;
+                    fact[i] = prod % 10;
+                    carry = prod / 10;
+                }
+
+
+                while (carry != 0)
+                {
+                    fact[size] = carry % 10;
+                    carry = carry / 10;
+                    size++;
+                }
+            }
+            Console.Write("Фарториал: ");
+            for (int i = size - 1; i >= 0; i--)
+            {
+                Console.Write(fact[i]);
+            }
+
+
+        }
+    }
+}
